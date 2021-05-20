5d7803e8a138a7f143be5db8072a5dd4b474daab
DaYoung21 <69308065+DaYoung21@users.noreply.github.com>
2021-05-21 03:30:35 +0900

    Update 6번.c

--- C/7장/6번.c
+++ C/7장/6번.c
@@ -3,42 +3,37 @@
 int main()
 {
 	char op;
-	int result, x, y;
+	int x, y;
+
+	printf("*****************\n");
+	printf("A----Add\n");
+	printf("S----Sub\n");
+	printf("M----Multi\n");
+	printf("D----Did\n");
+	printf("Q----Quit\n");
+	printf("*****************\n");
 
 	do
 	{
-		printf("*****************\n");
-		printf("A----Add\n");
-		printf("S----Sub\n");
-		printf("M----Multi\n");
-		printf("D----Did\n");
-		printf("Q----Quit\n");
-		printf("*****************\n");
-
 		printf("연산을 선택하시오 :");
 		scanf("\n%c", &op);
+
+		if (op == 'Q')
+			break;
 
 		printf("두수를 공백으로 분리하여 입력하시오:");
 		scanf("%d %d", &x, &y);
 
-
 		if (op == 'A')
-			result = x + y;
-		
+			printf("%d\n", x + y);
 		if (op == 'S')
-			result = x * y;
-			
+			printf("%d\n", x - y);
 		if (op == 'M')
-			result = x / y;
-			
+			printf("%d\n", x * y);
 		if (op == 'D')
-			result = x % y;
-			
-		if (op == 'Q')
-			break;
+			printf("%d\n", x / y);
 
-		printf("%d\n", result);
 	} while (1);
 
-		return 0;
+	return 0;
 }
